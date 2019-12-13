72f7912a6926a0a697de13b8ddf83054af59d547
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2019-12-13 13:05:48 +0330

    enva commands are specified in a separated file

--- src/enva/types.d.ts
+++ src/enva/types.d.ts
@@ -1,14 +1,17 @@
 interface StatusWithMessage {
-  status: boolean,
-  message: string
+    status: boolean
+    message: string
 }
 
+type TEnvaCommand = () => void
+type TEnvaCommandList = Record<string, TEnvaCommand>
+
 interface ConfigObject {
-  commands: Object,
-  plugins: [
-    {
-      name: string,
-      command: string
-    }
-  ]
+    commands: Object
+    plugins: [
+        {
+            name: string
+            command: string
+        }
+    ]
 }
